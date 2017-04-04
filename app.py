9bb6f2f6ab799717b57e65029de0cd76645de1c3
FrobozzElectric <gpfau7@gmail.com>
2017-04-04 11:52:28 -0400

    debug

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def get_stats():
         raise
     return jsonify(stats)
 
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
