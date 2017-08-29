c5697b8770da6677379d49c8f344d22d0adfc222
FrobozzElectric <gpfau7@gmail.com>
2017-08-29 18:18:38 -0400

    GET and POST

--- app.py
+++ app.py
@@ -6,30 +6,40 @@ from flask import abort, Flask, jsonify, make_response, request, Response
 app = Flask(__name__)
 
 
+def parser(args):
+    json_args = {'query', 'command' ,'sort'}
+    for arg in args:
+        if arg in json_args:
+            args[arg] = json.loads(args[arg][0])
+        else:
+            args[arg] = args[arg][0]
+    return args
+
+
 def abort_request(message, status):
     abort(make_response(jsonify(error=message), status))
 
 
-def query(collection, request, data):
+def query(collection, args, data):
     sort = None
-    if request.args.get('sort'):
-        sort_raw = json.loads(request.args.get('sort'))
+    if args.get('sort'):
+        sort_raw = args.get('sort')
         sort = []
         for key, value in sort_raw.items():
             sort.append((key, value))
 
     limit = None
     if request.args.get('limit'):
-        limit = int(request.args.get('limit'))
+        limit = int(args.get('limit'))
 
-    query = json.loads(request.args.get('query'))
+    query = args.get('query')
     data['results'].append(list(
             mongo_find(collection, query, sort, limit)))
     return data
 
 
-def command(db, request, data):
-    command = json.loads(request.args.get('command'))
+def command(db, args, data):
+    command = args.get('command')
     data['results'].append(db.command(command))
     return data
 
@@ -59,28 +69,33 @@ def healthcheck():
     return json_resp({'error': None}, 200)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def connection_string():
-    if not request.args.get('uri'):
+    if request.method == 'POST':
+        args = request.get_json()
+    else:
+        args = parser(dict(request.args))
+
+    if not args.get('uri'):
         abort_request('missing \'uri\' parameter', 422)
 
-    if not request.args.get('query') and not request.args.get('command'):
+    if not args.get('query') and not args.get('command'):
         abort_request('missing \'query\' or \'command\'', 422)
 
-    uri = request.args.get('uri').strip('"').strip("'")
+    uri = args.get('uri').strip('"').strip("'")
     try:
         client = MongoClient(uri, serverSelectionTimeoutMS=5000)
         data = {'error': None, 'results': []}
         db = client.get_default_database()
 
-        if request.args.get('query'):
-            if not request.args.get('collection'):
+        if args.get('query'):
+            if not args.get('collection'):
                 abort_request('missing \'collection\' parameter', 422)
-            collection = db[request.args.get('collection')]
-            data = query(collection, request, data)
+            collection = db[args.get('collection')]
+            data = query(collection, args, data)
 
-        if request.args.get('command'):
-            data = command(db, request, data)
+        if args.get('command'):
+            data = command(db, args, data)
 
         client.close()
     except Exception as error:
